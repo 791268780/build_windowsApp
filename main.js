b5cb205cb05725f29e709bf230646a77271a1675
“wcj <791268780@qq.com>
2021-04-14 18:24:42 +0800

    2

--- main.js
+++ main.js
@@ -1,53 +1,33 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
- 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win = null
- 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
- 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '/to_build_file/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
- 
-  // Open the DevTools.
-  // win.webContents.openDevTools()
- 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
- 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
- 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
- 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+const {app, BrowserWindow} = require('electron');
 
+function createWindow() {
+  // 创建浏览器窗口。
+  const win = new BrowserWindow({
+    width: 800,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true,  // 默认隐藏菜单栏
+    fullscreen: false,       // 默认全屏
+  });
+
+  // 加载index.html文件
+  win.loadFile('./to_build_file/index.html');
+}
+
+// Electron 会在初始化后并准备创建浏览器窗口时，调用这个函数。部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow);
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
